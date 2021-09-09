c599abd161ea7cebf189584d5362494d4352b2c7
Dima Vaitsiachovski <boretskiy70@bk.ru>
2021-09-09 11:01:49 +0300

    feat: finish

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) !== "string" || isNaN(parseFloat(sampleActivity)) || sampleActivity <= 0 || sampleActivity >= 15) return false;
+  let first = MODERN_ACTIVITY / sampleActivity;
+  let k = 0.693 / 5730;
+  return Math.ceil(Math.log(first) / k);
 }
+dateSample('9.133070472061402');
