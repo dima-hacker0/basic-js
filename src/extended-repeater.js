430385fb3ff26d11731bce7ab73bfae7a1f84780
Dima Vaitsiachovski <boretskiy70@bk.ru>
2021-09-12 17:32:15 +0300

    feat: finish

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,71 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  if (options.additionRepeatTimes == undefined) options.additionRepeatTimes = 1;
+  if (options.repeatTimes == undefined) options.repeatTimes = 1;
+  if (options.addition === undefined) options.addition = '';
+  str = String(str);
+  let finish = '';
+  if(!options.separator && !options.addition && !options.additionRepeatTimes && !options.additionSeparator) {
+    for(let i = 0; i < options.repeatTimes; i++) {
+      finish += str;
+      if(i != options.repeatTimes -1) finish += '+';
+    }
+    return finish;
+  }
+
+  if(!options.separator && !options.additionSeparator) {
+    for(let i = 0; i < options.repeatTimes; i++) {
+      finish += str;
+      for(let j = 0; j < options.additionRepeatTimes; j++) {
+        if(j == options.additionRepeatTimes - 1) finish += options.addition;
+        else finish += `${options.addition}|`;
+      }
+      if(i !=  options.repeatTimes - 1) finish += '+';
+    }
+    return finish;
+  }
+  if(!options.additionSeparator) {
+    for(let i = 0; i < options.repeatTimes; i++) {
+      finish += str;
+      for(let j = 0; j < options.additionRepeatTimes; j++) {
+        if(j == options.additionRepeatTimes - 1) finish += options.addition;
+        else finish += `${options.addition}|`;
+      }
+      if(i !=  options.repeatTimes - 1) finish += options.separator;
+    }
+    return finish;
+  }
+  if(!options.separator) {
+    for(let i = 0; i < options.repeatTimes; i++) {
+      finish += str;
+      for(let j = 0; j < options.additionRepeatTimes; j++) {
+        finish += options.addition;
+        if(j != options.additionRepeatTimes - 1) finish += options. additionSeparator;
+      }
+      if(i != options.repeatTimes - 1) finish += '+';
+    }
+    return finish;
+  }
+
+
+  if(!options.repeatTimes) {
+    finish += str;
+    finish += options.addition;
+    return finish;
+  }
+  for(let i = 0; i < options.repeatTimes; i++) {
+    finish += str;
+    for(let j = 0; j < options.additionRepeatTimes; j++) {
+      finish += options.addition;
+      if(j != options.additionRepeatTimes - 1) finish += options. additionSeparator;
+    }
+    if(i != options.repeatTimes - 1) finish += options.separator;
+  }
+  return finish;
 }
+const objWithSpecificCoercion = {
+  [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
+};
+repeater(objWithSpecificCoercion, { repeatTimes: 2, addition: objWithSpecificCoercion });
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,13 +19,82 @@ import { NotImplementedError } from '../extensions/index.js';
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+ export default class VigenereCipheringMachine {
+  constructor(reverse) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  encrypt(str, word) {
+    if(!str || !word) throw new Error('Incorrect arguments!');
+    str = str.toUpperCase();
+    word = word.toUpperCase()
+    let arrStr = str.split('');
+    let arrWord = []
+    let temp = 0
+    const A = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    for(let i = 0; i < arrStr.length; i++) {
+      arrWord[i] = word[temp];
+      temp++;
+      if(temp == word.length) temp = 0;
+    }
+    let codeFigures = [];
+    temp = 0;
+    for(let i = 0; i < arrStr.length; i++) {
+      if(A.indexOf(arrStr[i]) !== -1) {
+        codeFigures[i] = A.indexOf(arrStr[i]) + A.indexOf(arrWord[temp]);
+        temp++;
+        if(temp == word.length) temp = 0;
+      }
+      else{
+        codeFigures[i] = str[i];
+        continue;
+      }
+    }
+    for(let i = 0; i < codeFigures.length; i++) {
+      if(codeFigures[i] > 25) codeFigures[i] -= 26;
+    }
+    for(let i = 0; i < codeFigures.length; i++) {
+      if(typeof codeFigures[i] === 'number') codeFigures[i] = A[codeFigures[i]];
+    }
+    if(this.reverse === false) return codeFigures.reverse().join('');
+    else return codeFigures.join('');
+  }
+
+  decrypt(str, word) {
+    if(!str || !word) throw new Error('Incorrect arguments!');
+    str = str.toUpperCase();
+    word = word.toUpperCase()
+    let arrStr = str.split('');
+    let arrWord = []
+    let temp = 0
+    const A = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    for(let i = 0; i < arrStr.length; i++) {
+      arrWord[i] = word[temp];
+      temp++;
+      if(temp == word.length) temp = 0;
+    }
+    let codeFigures = [];
+    temp = 0;
+    for(let i = 0; i < arrStr.length; i++) {
+      if(A.indexOf(arrStr[i]) !== -1) {
+        codeFigures[i] = A.indexOf(arrStr[i]) - A.indexOf(arrWord[temp]);
+        temp++;
+        if(temp == word.length) temp = 0;
+      }
+      else{
+        codeFigures[i] = str[i];
+        continue;
+      }
+    }
+    for(let i = 0; i < codeFigures.length; i++) {
+      if(codeFigures[i] < 0) codeFigures[i] += 26  ;
+    }
+    for(let i = 0; i < codeFigures.length; i++) {
+      if(typeof codeFigures[i] === 'number') codeFigures[i] = A[codeFigures[i]];
+    }
+    if(this.reverse === false) return codeFigures.reverse().join('');
+    else return codeFigures.join('');
   }
 }
