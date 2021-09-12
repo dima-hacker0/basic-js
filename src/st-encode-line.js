3ae9523e998a25e333770b37ddf64640966871b6
Dima Vaitsiachovski <boretskiy70@bk.ru>
2021-09-12 23:46:41 +0300

    feat: finish

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,25 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let arr = [];
+  let kolvo = 0;
+  let arrShet = 0;
+  for(let i = 0; i < str.length;) {
+    let temp = str[i];
+    while(temp == str[i]){
+      kolvo++;
+      i++;
+    }
+    arr[arrShet] = kolvo;
+    kolvo = 0;
+    arrShet++;
+    arr[arrShet] = str[i - 1];
+    arrShet++;
+  }
+  for(let i = 0; i < arr.length; i++) 
+    if(arr[i] == 1) arr.splice(i, 1);
+  for(let i = 0; i < arr.length; i++) arr[i] = String(arr[i]);
+  return arr.join('');
 }
+encodeLine('abbcca');
