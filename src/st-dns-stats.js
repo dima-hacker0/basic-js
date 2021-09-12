beb5989e54473b812a456527b0290d2cca8abb0a
Dima Vaitsiachovski <boretskiy70@bk.ru>
2021-09-12 23:18:27 +0300

    feat: finish

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -22,7 +22,41 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  let result = {};
+  let arr = [];
+  for(let i = 0; i < domains.length; i++) {
+    arr[i] = domains[i].split('.');
+  } 
+  for(let i = 0; i < domains.length; i++) arr[i].reverse();
+  for(let i = 0; i < domains.length; i++) {
+    for(let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = '.' + arr[i][j];
+    }
+  }
+  let arrFin = [];
+  let arrFinShet = 0;
+  let temp = 1;
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) sum += arr[i].length;
+  for(let i = 0; i < sum; i++) arrFin[i] = '';
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < temp; j++) {     
+      arrFin[arrFinShet] += arr[i][j]; 
+      if(j + 1 == temp) {
+        j = -1;
+        arrFinShet++;
+        temp++;
+      }
+      if(temp == arr[i].length + 1) break; 
+   }
+   temp = 1; 
+  }
+  for(let i = 0; i < sum; i++) {
+    if(arrFin[i] in result) result[`${arrFin[i]}`]++;
+    else result[`${arrFin[i]}`] = 1;
+  }
+  return result;
 }
+getDNSStats(['code.yandex.ru', 'music.yandex.ru', 'yandex.ru'])
+//{'.ru': 3, '.ru.yandex': 3, '.ru.yandex.code': 1,'.ru.yandex.music': 1}
