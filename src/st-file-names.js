36c107eeb41668f506ba724575e40b94f523d072
Dima Vaitsiachovski <boretskiy70@bk.ru>
2021-09-14 22:27:04 +0300

    feat: finish

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -19,13 +19,20 @@ export default function renameFiles(names) {
   let arrNew = [];
   let temp;
   let count;
+  let skob1 = 0;
+  let skob2 = 0;
   for(let i = 0; i < names.length; i++) {
     if(i == 0) arrNew[0] = names[0];
     temp = names[i];
     for(let j = 0; j < i; j++) {
-      if(arrNew[j].includes(names[i])) {
-        
-        count++;
+      if(arrNew[j].includes(names[i])) { 
+          for(let l = 0; l < arrNew[j].length; l++) {
+            if(arrNew[j][l] == '(') skob1++;
+          }
+          for(let l = 0; l < names[i].length; l++) {
+            if(names[i][l] == '(') skob2++;
+          }
+        if(skob1 - skob2 < 2) count++
       } 
     }
     if(count > 0) arrNew[i] = names[i] + `(${count})`;
